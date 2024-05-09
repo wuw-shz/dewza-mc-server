a804e74dbb943aecde7350c5d8a67e7c12566cbf
wuw-shz <wuw62481@gmail.com>
2024-05-09 20:17:56 +0700

    Update !build.bat

--- !build.bat
+++ !build.bat
@@ -1,15 +1,14 @@
 @echo off
 
-set zip=mods-4.3.zip
+set zip=mods-4.4.zip
 
-if exist %zip% (
-    del %zip%
+if exist builds/%zip% (
+    del /q builds\%zip%
 )
 
 PowerShell -Command "Compress-Archive -Path mods -DestinationPath client-mods/mods.zip" >nul
 PowerShell -Command "Compress-Archive -Path client-mods -DestinationPath builds/%zip%" >nul
 
-cd client-mods
-del /q *.zip
+del /q client-mods\mods.zip
 
 echo ✅  Mods Built Successfully.
